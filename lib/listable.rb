ad8a2632d2bff28a6be9811c72fbcf203a50de72
Malki Davis <md01@me.com>
2016-08-27 22:51:30 +0300

    move format priority to listable module

--- lib/listable.rb
+++ lib/listable.rb
@@ -15,6 +15,11 @@ module Listable
     end
   end
 
-  # def format_priority
-  # end
+  def format_priority(priority)
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
 end
